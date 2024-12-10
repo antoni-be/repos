3bc0a86ebbf6ad0f0e3be1d19c44903114e6d81c
antonibe@student.agh.edu.pl <antonibe@student.agh.edu.pl>
2024-12-10 19:38:36 +0100

    poprawki

--- OstanieZadanie/OstanieZadanie/Program.cs
+++ OstanieZadanie/OstanieZadanie/Program.cs
@@ -16,42 +16,84 @@ namespace OstanieZadanie
              *     a. jaki menadzer musi zatwierdzić
              *     b. kto może nadać dostęp do aplikacji
              */
+            List<User> UserList = new List<User>();
+            List<App> AppList = new List<App>();
+
+
             App Visio = new App(1, "Visio");
+            AppList.Add(Visio);
+
             App SQL = new App(2, "SQL");
+            AppList.Add(SQL);
+
             App Studio = new App(3, "Studio");
+            AppList.Add(Studio);
+
             App Postman = new App(4, "Postman");
+            AppList.Add(Postman);
+
 
             User Karolina = new User("Karolina", 1, 11);
             Karolina.GrantAccess(4, 5, 6);
+            UserList.Add(Karolina);
+
             User Ola = new User("Ola", 2, 11);
             Ola.GrantAccess(1, 2, 3);
+            UserList.Add(Ola);
+
             User Artur = new User("Artur", 3, 2);
             Artur.GrantAccess(1, 2);
+            UserList.Add(Artur);
+
             User Łukasz = new User("Łukasz", 4, 2);
             Łukasz.GrantAccess(1, 2);
+            UserList.Add(Łukasz);
+
             User Andrzej = new User("Łukasz", 5, 2);
             Andrzej.GrantAccess(3, 4);
-
-            List<User> UserList = new List<User>();
-            UserList.Add(Karolina);
-            UserList.Add(Ola);
-            UserList.Add(Artur);
-            UserList.Add(Łukasz);
             UserList.Add(Andrzej);
 
-            List<App> AppList = new List<App>();
-            AppList.Add(Visio);
-            AppList.Add(SQL);
-            AppList.Add(Studio);
-            AppList.Add(Postman);
+            User Wiktor = new User("Wiktor", 6, 2);
+            Wiktor.GrantAccess(3, 4);
+            UserList.Add(Wiktor);
+
+            User Rafał = new User("Rafał", 7, 1);
+            Rafał.GrantAccess(5, 6);
+            UserList.Add(Rafał);
+
+            User Michał = new User("Michał", 8, 1);
+            Michał.GrantAccess(5, 6);
+            UserList.Add(Michał);
+
+            User Damian = new User("Damian", 9, 1);
+            UserList.Add(Damian);
+
+            User Paweł = new User("Paweł", 10, 1);
+            UserList.Add(Paweł);
+
+            User Steven = new User("Steven", 11, 99);
+            UserList.Add(Steven);
+
+
 
             Console.Write("Podaj ID użytkownika któremu chcesz nadać dostęp: ");
             int UserID = Convert.ToInt32(Console.ReadLine());
             Console.Write("Podaj ID aplikacji do której chcesz nadać dostęp: ");
             int AppID = Convert.ToInt32(Console.ReadLine());
 
+            User SearchedUser = UserList.FirstOrDefault(user => user.ID == UserID);
+            if (SearchedUser != null)
+            {
+                User Manager = UserList.FirstOrDefault(manager => manager.ID.Equals(SearchedUser.MngID));
+                if (Manager != null)
+                {
+                    Console.WriteLine($"Wymagane zatwierdzenie od mendadżera: {Manager.Name}");
+                }
+                else { Console.WriteLine("Error: Nie przypisano menadżerki!"); }
+            }
+            else { Console.WriteLine("Error: Nie odnaleziono użytkownika o takim ID w bazie!"); }
 
-            foreach (var employee in UserList)
+            /*foreach (var employee in UserList)
             {   //wyszukujemy pracownika
                 if (employee.ID == UserID)
                 {
@@ -61,36 +103,15 @@ namespace OstanieZadanie
                     {
                         if (manager.ID == ManagerID)
                         {
-                            Console.WriteLine($"Wymagane zatwierdzenie od: {manager.Name}");
+                            Console.WriteLine($"Wymagane zatwierdzenie od menadżerki: {manager.Name}");
                         }
                     }
                 }
-            }
-            List<User> EmployeesWithAccess = new List<User>();
-            string AppSearch = "";
+            }*/
 
-            //szukamy na liście aplikacji
-            foreach (var app in AppList)
-            {   //sprawdzamy czy ID się zgadza
-                if (AppID == app.ID) 
-                {   //szukamy na liście użytkowników osoby z dostępem
-                    AppSearch = app.Name;
-                    foreach (var admin in UserList) 
-                    {
-                        foreach (var accesses in admin.AccesibleApps)
-                        {
-                            if (admin.AccesibleApps.Contains(AppID))
-                            {
-                                EmployeesWithAccess.Add(admin);
-                            }
-                        }
-                    }
-                }
-            }
-
-            Console.WriteLine($"W celu uzyskania dostępu do {AppSearch} wymagane potwierdzenie od:");
-            foreach (var employee in EmployeesWithAccess) { Console.WriteLine($"{employee.Name}"); }
-
+            List<User> RequiredAccess = UserList.Where(admin => admin.AccesibleApps.Contains(AppID)).ToList();
+            Console.WriteLine("Wymagana przydzielenie licencji przez jednego z administratorów:");
+            foreach (var admin in RequiredAccess) { Console.WriteLine($"- {admin.Name}"); }
 
             Console.ReadLine();
 
